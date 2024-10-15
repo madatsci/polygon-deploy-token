2d6e91ecd404aa2cce725500d9060be43c639c44
Andrei Shevkaliuk <sedemaqu@gmail.com>
2024-10-15 13:05:47 +0300

    Initial commit

---
+++ contracts/Factory.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Factory {
+    event TokenCreated(address tokenAddress);
+
+    function deployToken(string memory name, string memory symbol, uint8 decimals, uint256 initialSupply) public {
+        Token t = new Token(name, symbol, decimals);
+        t.mint(msg.sender, initialSupply);
+        emit TokenCreated(address(t));
+    }
+}
